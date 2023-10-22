842832c8985d3c7543bb3ebea08d69f0247f40e9
Achylis21 <francisco.danielivan021@gmail.com>
2023-10-22 18:49:22 +0800

    Added Changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,33 @@
 import blogLogo from './img/logo.png';
+import "./Styles/Navbar.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 
 function Navbar(){
     return (
         <header>
-            <h3><img src={ blogLogo } alt="logo" width={170} height={35}/></h3>
-            <nav>
-                <ul>
-                    <li><a href='#Home'>Home</a></li>
-                    <li><a href='#Bio'>Biography</a></li>
-                    <li><a href='#Skills'>Technical Skills</a></li>
-                    <li><a href='#Educ'>Educational Attainment</a></li>
-                    <li><a href='#Certif'>Certification</a></li>
-                    <li><a href='#More'>More...</a></li>
-                </ul>
+            <nav className="navbar fixed-top navbar-expand-lg px-5">
+                <div className="container-fluid" id='navbar'>
+                    <span className="navbar-brand"><img src={ blogLogo } alt="logo" width={170} height={35}/></span>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item"><a id='navLink' className="nav-link" href='#Home'>Home</a></li>
+                            <li className="nav-item"><a id='navLink' className="nav-link" href='#Bio'>Biography</a></li>
+                            <li className="nav-item"><a id='navLink' className="nav-link" href='#Skills'>Technical Skills</a></li>
+                            <li class="nav-item dropdown">
+                            <a id='navLink' class="nav-link dropdown-toggle" href="#a" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                More...
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a className="dropdown-item" href="#b">Educational Attainment</a></li>
+                                <li><a className="dropdown-item" href="#c">Certifications</a></li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li><span className="dropdown-item">to be updated...</span></li>
+                            </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </header>
     );
