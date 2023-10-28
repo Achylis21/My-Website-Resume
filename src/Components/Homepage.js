f5140ca6119264054acfa58dbb5fadebcf4ebbbb
Achylis21 <francisco.danielivan021@gmail.com>
2023-10-28 20:20:41 +0800

    New Patch HAHA

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
 import './Styles/Homepage.css';
-import Profile from './img/Profile.jpg'
+import HomeContent from './homeContent';
+import Profile from './img/Profile.jpg';
 
 function Homepage() {
     const handleClick = (url) => {
@@ -13,26 +14,51 @@ function Homepage() {
         }
     }
     return(
-        <div id='wavyImage'>
-            <div className='container'>
-                <div class="card mb-3" id='sentence'>
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="card-body bg-transparent">
-                                <h1>Hi! I am Daniel,</h1>
-                                <p>An undergraduate aspiring developer.</p>
-                                <button className='btn btn-outline-light' onClick={() => {handleClick(1)}}>LinkedIn</button>
-                                <button className='btn btn-outline-light' onClick={() => {handleClick(2)}}>Github</button>
-                                <button className='btn btn-outline-light'>Email Me!</button>
+        <div id='Homepage'>
+            <div id='wavyImage'>
+                <div className='container'>
+                    <div class="card mb-3" id='sentence'>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="card-body bg-transparent">
+                                    <h1>Hi! I am Daniel,</h1>
+                                    <p>A graduate with a degree in Computer Science. I am an aspiring MERN developer.</p>
+                                    <button className='btn btn-outline-light' onClick={() => {handleClick(1)}}>LinkedIn</button>
+                                    <button className='btn btn-outline-light' onClick={() => {handleClick(2)}}>Github</button>
+                                    <button className='btn btn-outline-light'>Contact me!</button>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <img src={ Profile } class="img-fluid" alt="Profile" width={300} height={500}/>
                             </div>
                         </div>
-                        <div class="col-sm-4">
-                            <img src={ Profile } class="img-fluid" alt="Profile" width={300} height={500}/>
-                        </div>
                     </div>
-                    </div>
+                </div>
             </div>
-        </div>
+            <div id='aboutme'>
+                <div className='card'>
+                    <div className='card-header-primary'>
+                        <h2 className='card-title'>About me!</h2>
+                    </div>
+                    <div className='card-body'>
+                        <p className='card-text'><HomeContent /></p>
+                    </div>
+                </div>
+            </div>
+            <div id='Personal'>
+                <div className='container'>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                </div>
+            </div>
+    </div>
     );
 }
 export default Homepage;
---
+++ src/Components/homeContent.js
@@ -0,0 +1,14 @@
+const HomeContent = (section) => {
+    return(
+        <div className="About me">
+            <p>
+            I am a graduate of Bachelor of Science in Computer Science Major in Application Development at the University of Makati who want to pursue a career in software development and gain a challenging position that fully utilizes my skills and provides me with appropriate opportunities to grow my technical and communication skills
+            <br/>
+            <br/>
+            I am an aspiring developer who graduated from the University of Makati with a bachelor's degree in computer science with a major in application development. I have much experience in Java, HTML, CSS, JavaScript, and C# (Unity) programming and am willing to learn new programming languages and techniques. I am also knowledgeable about basic Microsoft applications such as Word, Excel, and Google Docs.
+            </p>
+        </div>
+    );
+}
+
+export default HomeContent;
